a83ad329b8b7eab9fa1dea03f173190edd5e1fb5
Eduardo Martin <eduardomartin.mail@gmail.com>
2022-10-12 14:05:44 -0700

    made map function better

--- functy.py
+++ functy.py
@@ -2,6 +2,7 @@
 
 #map(action, [1,2,3])
 
+# this returns an object location
 def verb(arg):
     ary = []
     for i in arg:
@@ -9,3 +10,9 @@ def verb(arg):
     return ary
 
 print(map(verb,[1,2,3]))
+
+#this is how map returns a list
+def verb(arg):
+  return arg*2
+
+print(list(map(verb,[1,2,3])))
